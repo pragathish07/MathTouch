a1cfc36f93f9c478bbf23f63af0ca0537cf16371
pragathish07 <pragarajesh779jd@gmail.com>
2024-10-27 20:25:52 +0530

    patch up

--- server/main.py
+++ server/main.py
@@ -29,5 +29,5 @@ async def root():
 app.include_router(calculator_router, prefix="/calculate", tags=["calculate"])
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host=SERVER_URL, port=int(PORT), reload=(ENV == "dev"))
+""" if __name__ == "__main__":
+    uvicorn.run("main:app", host=SERVER_URL, port=int(PORT), reload=(ENV == "dev")) """
